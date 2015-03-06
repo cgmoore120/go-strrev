d29965ae3010fb421e9366b22460bdfbcda76332
Chris Moore <chris@cloudspace.com>
2015-03-06 12:00:01 -0500

    added Reverse function instead of using example github package import

--- go-strrev.go
+++ go-strrev.go
@@ -5,11 +5,18 @@ import(
       "unicode/utf8"
       "encoding/json"
       "os"
-      "github.com/golang/example/stringutil"
 )
 
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 func main() {
-  byte_map := map[string]int{"reverse" : string(stringutil.Reverse(os.Args[1]))}
+  byte_map := map[string]int{"reverse" : string(Reverse(os.Args[1]))}
 
   result, _ := json.Marshal(byte_map)
   fmt.Println(string(result))
