bef1e27de36fd24d01f394485ffcb11fe484bea5
Chris Moore <chris@cloudspace.com>
2015-03-06 12:13:22 -0500

    missed a double quote

--- go-strrev.go
+++ go-strrev.go
@@ -14,5 +14,5 @@ func Reverse(s string) string {
 }
 
 func main() {
-  fmt.Println("{\"reversed\" : \"" +Reverse("osArgs[1])+ "\"}")
+  fmt.Println("{\"reversed\" : \"" +Reverse(osArgs[1])+ "\"}")
 }
