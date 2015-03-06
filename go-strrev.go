f2d499440e46362fce039b55709b8ec7a74e05b8
Chris Moore <chris@cloudspace.com>
2015-03-06 12:02:51 -0500

    syntax fixes

--- go-strrev.go
+++ go-strrev.go
@@ -8,16 +8,17 @@ import(
 )
 
 func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return string(r)
+        r := []rune(s)
+        for i, j := 0, utf8.RuneCountInString(r)-1; i < utf8.RuneCountInString(r)/2; i, j = i+1, j-1 {
+                r[i], r[j] = r[j], r[i]
+        }
+        return string(r)
 }
 
 func main() {
-  byte_map := map[string]int{"reverse" : string(Reverse(os.Args[1]))}
+  byte_map := map[string]int{"reverse" : Reverse(os.Args[1])}
 
   result, _ := json.Marshal(byte_map)
   fmt.Println(string(result))
 }
+
